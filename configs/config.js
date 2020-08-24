7147e9053f045df08b16d3226ee09b16b9e0e5f0
cheng cui <tonycui0401@gmail.com>
2020-08-24 07:52:58 +0100

    ddd

--- configs/config.js
+++ configs/config.js
@@ -1,9 +1,5 @@
-require("dotenv").config();
-const dbHost = process.env.DB_HOST;
-const dbName = process.env.DB_NAME;
-const dbUsername = process.env.DB_USERNAME;
-const dbPassword = process.env.DB_PASSWORD;
-const dbPort = process.env.DB_PORT || 5432;
+
+
 
 module.exports = {
 
